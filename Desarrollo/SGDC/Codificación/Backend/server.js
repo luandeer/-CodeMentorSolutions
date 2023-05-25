7afbaaa2322e2d6fc9e3ab9ae3b76efb819050cd
Santiago <santiago.cumpa@unmsm.edu.pe>
2023-05-25 02:11:06 -0500

    prueba de servidor en server.js

---
+++ Desarrollo/SGDC/Codificación/Backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+   res.send('Primera solicitud');
+});
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+   console.log(`Servidor web funcionando en https://localhost:${port}`);
+})
