a3ac39cc38bdf90098f3215588908f0e6e34efb9
SanchezHidalgo <gerardosanh1@gmail.com>
2023-05-30 13:18:58 -0500

    Conexión a la BD

--- Desarrollo/SGDC/Codificación/conexion.php
+++ Desarrollo/SGDC/Codificación/conexion.php
@@ -1,14 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$database = "SGDC_DB";
-
-// Crea la conexión
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Verifica si hay errores de conexión
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+function dbQuery($sql) {
+    // Se declara una función para poder conectarnos
+    $DBHost = "localhost"; //"127.0.0.1"
+    $DBname = "BD_SGDC"; //Nombre de la base de datos que utilizaremos para la TA
+    $DBUser = "root"; // El usuario por defecto es el root
+    $DBPass = "root"; // Esta es la contraseña por defecto
+    $connDB = mysqli_connect("$DBHost","$DBUser","$DBPass", $DBname);
+    if (mysqli_connect_errno())  { 
+      echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+    }
 }
 ?>
