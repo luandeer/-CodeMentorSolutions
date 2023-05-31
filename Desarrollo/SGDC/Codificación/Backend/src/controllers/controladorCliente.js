08926857ebd3195c60429de77b39bce35bca38f9
Santiago <santiago.cumpa@unmsm.edu.pe>
2023-05-31 15:28:07 -0500

    implementacion base para el archivo controladorCliente.js

---
+++ Desarrollo/SGDC/Codificación/Backend/src/controllers/controladorCliente.js
@@ -0,0 +1,11 @@
+const conexion = require('./database/database');
+
+const registrarUsuario = (req, res) => {
+   let { nombre, apellido, cel, dir, contra } = req.body;
+
+   const query = `INSERT INTO clients (nombres, apellidos, celular, direccion, password) VALUES ("${nombre}", "${apellido}", "${cel}", "${dir}", "${contra}")`;
+
+   console.log('Usuario registrado');
+}
+
+module.exports = registrarUsuario;
--- Desarrollo/SGDC/Codificación/Backend/src/server.js
+++ Desarrollo/SGDC/Codificación/Backend/src/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const path = require('path');
 
-const conexion = require('./database/database');
-
 // configuracion
 const app = express();
 app.use(express.json());
