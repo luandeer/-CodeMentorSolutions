6d686232d409a5f15eb32f30eec556efad652401
Airton <airton.collachagua@unmsm.edu.pe>
2023-06-27 21:35:57 -0500

    Inicio de sesion envio de formulario

--- Desarrollo/SGDC/Codificación/Backend/controllers/controlCliente.js
+++ Desarrollo/SGDC/Codificación/Backend/controllers/controlCliente.js
@@ -14,5 +14,24 @@ const regitrarUsuario = (req, res) => {
    });
    res.redirect('/');
 }
-
+//Inicio de sesión
+router.post('/login', (req, res) => {
+   const { celular, password } = req.body;
+ 
+   // Realizar la lógica de verificación de credenciales en la base de datos
+   const sql = 'SELECT * FROM clients WHERE celular = ? AND contraseña = ?';
+   const values = [celular, password];
+ 
+   db.query(sql, values, (err, result) => {
+     if (err) {
+       throw err;
+     }
+   // Verificar si se encontró un usuario con las credenciales proporcionadas
+     if (result.length > 0) {
+       res.status(200).json({ message: 'Inicio de sesión exitoso' });
+     } else {
+       res.status(401).json({ message: 'Credenciales inválidas' });
+     }
+   });
+ }); 
 module.exports = { regitrarUsuario };
