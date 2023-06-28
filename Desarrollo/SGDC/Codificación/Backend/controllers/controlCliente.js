83b6b022fbd8ebe024cc91d9ebe68ab60a78fde3
Airton <airton.collachagua@unmsm.edu.pe>
2023-06-27 21:20:47 -0500

    validaciones para el llenado de datos al registrar

--- Desarrollo/SGDC/Codificación/Backend/controllers/controlCliente.js
+++ Desarrollo/SGDC/Codificación/Backend/controllers/controlCliente.js
@@ -1,9 +1,10 @@
 const connection = require('../database/connection');
 
-const regitrarUsuario = (req, res) => {
-   let { dni, nombre, correo } = req.body;
 
-   const query = `INSERT INTO clients (dni, nombre, correo) VALUES ("${dni}", "${nombre}", "${correo}")`;
+const regitrarUsuario = (req, res) => {
+   let {nombre, apellido, celular, direccion, contraseña } = req.body;
+
+   const query = `INSERT INTO clients (nombre, apellido, celular, direccion, contraseña) VALUES ("${nombre}", "${apellido}", "${celular}", "${direccion}", "${contraseña}"  )`;
 
    connection.query(query, (err, results) => {
       if (err)
