0adc8e92adba3a7e000ed6dc1154ac697a15607d
Airton <airton.collachagua@unmsm.edu.pe>
2023-06-29 13:55:23 -0500

    Correccion de Inicio de sesion

--- Desarrollo/SGDC/Codificación/Backend/src/routers/routerCliente.js
+++ Desarrollo/SGDC/Codificación/Backend/src/routers/routerCliente.js
@@ -3,5 +3,6 @@ const routerCli = express.Router();
 const controladorCli = require('../controllers/controladorCliente');
 
 routerCli.post('/', controladorCli.registrarUsuario);
+routerCli.post('/login', controladorCli.iniciarSesion);
 
 module.exports = routerCli;
