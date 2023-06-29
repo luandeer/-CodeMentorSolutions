e7af1b64b1fe9759fa77633cbe5522b0fc300365
Airton <airton.collachagua@unmsm.edu.pe>
2023-06-29 13:57:38 -0500

    Correccion en inicio de sesion

--- Desarrollo/SGDC/Codificación/Backend/src/controllers/controladorCliente.js
+++ Desarrollo/SGDC/Codificación/Backend/src/controllers/controladorCliente.js
@@ -14,25 +14,32 @@ const registrarUsuario = (req, res) => {
 
    res.redirect('/');
 }
-//Inicio de sesión
-router.post('/login', (req, res) => {
-   const { celular, password } = req.body;
- 
-   // Realizar la lógica de verificación de credenciales en la base de datos
-   const sql = 'SELECT * FROM clientes WHERE celular = ? AND password = ?';
-   const values = [celular, password];
- 
-   db.query(sql, values, (err, result) => {
-     if (err) {
-       throw err;
-     }
-   // Verificar si se encontró un usuario con las credenciales proporcionadas
-     if (result.length > 0) {
-       res.status(200).json({ message: 'Inicio de sesión exitoso' });
-     } else {
-       res.status(401).json({ message: 'Credenciales inválidas' });
-     }
-   });
- }); 
-module.exports = router;
-module.exports = { registrarUsuario };
+const iniciarSesion = (req, res) => {
+  const { celular, password } = req.body;
+
+  const sql = 'SELECT * FROM clientes WHERE celular = ? AND password = ?';
+  const values = [celular, password];
+
+  conexion.query(sql, values, (err, result) => {
+    if (err) {
+      throw err;
+    }
+
+    if (result.length > 0) {
+      res.status(200).json({ message: 'Inicio de sesión exitoso' });
+    } else {
+      res.status(401).json({ message: 'Credenciales inválidas' });
+    }
+  });
+};
+module.exports = {
+  registrarUsuario,
+  iniciarSesion,
+};
+
+
+
+
+
+
+
